666339350031bda917b499e9dda141c6a353ca8d
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-09-29 21:10:17 -0700

    immutability, constants, overshadowing

---
+++ 03-common-language/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/ganyuxu/Documents/projects/rustlang-the-book/03-common-language/variables/target/debug/variables: /Users/ganyuxu/Documents/projects/rustlang-the-book/03-common-language/variables/src/main.rs
