33760a45413532c055c9cab5d2ace49e09a1ba77
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-09-29 21:11:03 -0700

    Revert "immutability, constants, overshadowing"
    This reverts commit 666339350031bda917b499e9dda141c6a353ca8d.

--- 03-common-language/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/ganyuxu/Documents/projects/rustlang-the-book/03-common-language/variables/target/debug/variables: /Users/ganyuxu/Documents/projects/rustlang-the-book/03-common-language/variables/src/main.rs
