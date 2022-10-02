23f577ead674f2c9ce746ae25b9ecaa264c8f6c6
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-10-02 16:26:23 -0700

    loop, while loop, for loop, range

--- 03-common-language/loops/src/main.rs
+++ 03-common-language/loops/src/main.rs
@@ -4,35 +4,73 @@
  * keyword will skip all remaining code in the code block and start the next
  * loop immediately
  */
-fn main() {
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;  // this works in similar fashion as "return"
-        }
-    }; // this loop block will evaluate to the expression in the break stmt
-    println!("The loop evaluates to {result}");
+fn fibloop(n: i32) {  // print the first n fibonacci numbers
+    let mut p1: i32 = 0;
+    let mut p2: i32 = 1;
+    let mut num: i32 = p1 + p2;
+    let mut count: i32 = 0;
     
-    let mut outercount: i32 = 0;
-    let mut innercount: i32;
-
-    'outerloop: loop { // the outer loop is given a label
-        innercount = 0;
-        loop {
-            println!("Loc is ({outercount}, {innercount})");
-            innercount +=1;
-
-            if innercount >= 1000 { break; }  // this break the innert loop
-            if outercount == 69 && innercount == 420 {
-                println!("Nice!");
-                break 'outerloop;  // this break out of the outer loop
-            }
+    loop {
+        if count >= n { break; }
+        if count == 0 {
+            println!("{p1}");
+        } else if count == 1 {
+            println!("{p2}");
+        } else {
+            println!("{num}");
+            p1 = p2;
+            p2 = num;
+            num = p1 + p2;
         }
-        outercount += 1;
+        count += 1;
     }
-    
-    // TODO: what about while loop?
-    // TODO: what about for loop?
+}
+
+fn fibwhile(n: i32) { // same as fibloop but using a while loop
+    let mut p1: i32 = 0;
+    let mut p2: i32 = 1;
+    let mut num: i32 = p1 + p2;
+    let mut count: i32 = 0;
+
+    while count < n {
+        if count == 0 {
+            println!("0");
+        } else if count == 1 {
+            println!("1");
+        } else {
+            println!("{num}");
+            p1 = p2;
+            p2 = num;
+            num = p1 + p2;
+        }
+        count += 1
+    }
+}
+
+fn fibfor(n: i32) { // same as fibloop but using a for loop
+    let mut p1: i32 = 0;
+    let mut p2: i32 = 1;
+    let mut num: i32 = p1 + p2;
+
+    for count in 0..n {  // this is the rust "range"
+        if count == 0 {
+            println!("0");
+        } else if count == 1 {
+            println!("1");
+        } else {
+            println!("{num}");
+            p1 = p2;
+            p2 = num;
+            num = p1 + p2;
+        }
+    }
+}
+
+fn main() {
+    println!("Using 'loop'");
+    fibloop(10);
+    println!("Using 'while'");
+    fibwhile(10);
+    println!("Using 'for'");
+    fibfor(10);
 }
