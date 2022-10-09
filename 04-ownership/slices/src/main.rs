81e5ce85231cfa41f639b0d62f5b93fc7a5a1e7a
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-10-08 21:10:32 -0700

    Examples of slices

--- 04-ownership/slices/src/main.rs
+++ 04-ownership/slices/src/main.rs
@@ -1,22 +1,40 @@
-/**
- * First let's build a function that returns the index of the first instance
- * of the input characther in the input string. If the input character does
- * not exist, return -1
- */
-fn strchr(s: &String, c: u8) -> usize {
+/** return the 'length' of the first word in the string */
+fn first_word_index(s: &String) -> usize {
     let bytes: &[u8] = s.as_bytes();
 
     for (i, &byte) in bytes.iter().enumerate() {
-        if byte == c {
+        if byte == b' ' {
             return i;
         }
     }
 
-    return 0;
+    return s.len();
 }
 
-fn main() {
-    let msg: String = String::from("Hello, world");
-    let chr: u8 = b' ';
-    println!("{}", strchr(&msg, chr));
+/** return an immutable slice */
+fn first_word_slice(s: &String) -> &str {
+    for (i, &byte) in s.as_bytes().iter().enumerate() {
+        if byte == b' ' { return &s[..i] }
+    }
+    return &s[..]
 }
+
+/**
+ * Return a slice of the input string that is the first
+ */
+
+fn main() {
+    // Get the first word by returning its length`
+    let msg: String = String::from("Hello world!");
+    let wordlen: usize = first_word_index(&msg);
+    let word: &str = &msg[..wordlen];
+    println!("{}", word);
+
+    // Get the first word by returning an immutable slice
+    println!("{}", first_word_slice(&msg));
+
+    let nums = [0, 1, 2, 3, 4];
+    let subarray = &nums[..4];
+    assert_eq!(subarray, &[0, 1, 2, 3]);
+}
+
