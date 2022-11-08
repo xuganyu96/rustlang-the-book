8e23caa89cbc4e8939ad662203843b4b35b5fbc0
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-11-07 20:39:21 -0800

    Didn't really learn closure very well

--- 13-functional-language-features/src/lib.rs
+++ 13-functional-language-features/src/lib.rs
@@ -1,3 +1,14 @@
-pub mod closure;
-pub mod iterator;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
